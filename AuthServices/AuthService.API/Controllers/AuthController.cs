4ff2804830884889c11076d02f24aea87d1138ba
Hemant Singh <87639735+hrana050@users.noreply.github.com>
2025-06-15 22:13:49 +0530

    auth service error resolved

--- AuthServices/AuthService.API/Controllers/AuthController.cs
+++ AuthServices/AuthService.API/Controllers/AuthController.cs
@@ -20,7 +20,6 @@ namespace AuthService.API.Controllers
         [Route("user/register")]
         public async Task<IActionResult> Register(RegisterUserCommand command)
         {
-            string name = 12345;
             var id = await _mediator.Send(command);
             return Ok(new { UserId = id });
         }
