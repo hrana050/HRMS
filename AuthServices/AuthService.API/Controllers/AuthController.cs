24417a4249956154bfd00810ad6cd214423d2589
Hemant Singh <87639735+hrana050@users.noreply.github.com>
2025-06-15 22:20:22 +0530

    changes in auth service

--- AuthServices/AuthService.API/Controllers/AuthController.cs
+++ AuthServices/AuthService.API/Controllers/AuthController.cs
@@ -20,6 +20,7 @@ namespace AuthService.API.Controllers
         [Route("user/register")]
         public async Task<IActionResult> Register(RegisterUserCommand command)
         {
+            // test CI trigger for AuthService
             var id = await _mediator.Send(command);
             return Ok(new { UserId = id });
         }
